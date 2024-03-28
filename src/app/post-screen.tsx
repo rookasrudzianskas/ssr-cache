0a58ba657543330caf98a48efcda8945ce15b2ad
rokas <rokas@rokas.com>
2024-03-28 08:09:49 +0200

    Refactor code to use object destructuring in usePosts()

--- src/app/post-screen.tsx
+++ src/app/post-screen.tsx
@@ -6,14 +6,14 @@ import {usePosts} from "@/src/hooks/post";
 import { useSWRConfig } from "swr"
 
 export default function PostScreen() {
-  const { posts, error, isLoading } = usePosts();
-  const { mutate } = useSWRConfig();
+  const { posts, error, isLoading, mutate } = usePosts();
+  // const { mutate } = useSWRConfig();
   if (isLoading) return <View className={'flex-1 items-center justify-center'}><ActivityIndicator /></View>
   if (error) return <View className={'flex-1 items-center justify-center'}><Text>Error: {error.message}</Text></View>
 
   // console.log(JSON.stringify(data, null, 2))
   const runMutation = async () => {
-    mutate('https://jsonplaceholder.typicode.com/posts', [])
+    await mutate()
   }
 
   return (
--- src/hooks/post.ts
+++ src/hooks/post.ts
@@ -2,10 +2,11 @@ import useSWR from "swr";
 import {fetcher} from "@/src/utils/fetcher";
 
 export const usePosts = () => {
-  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+  const { data, error, isLoading, mutate } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
   return {
     posts: data,
     error,
-    isLoading
+    isLoading,
+    mutate
   }
 }
